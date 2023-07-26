95e14881c3ba0796953fe7b66cd286c1125b6790
Milad Barzideh <milad.barzideh@snapp.cab>
2023-07-26 20:52:13 +0330

    slove intrest-is-interesting track

---
+++ interest-is-interesting/interest_is_interesting.go
@@ -0,0 +1,38 @@
+package interest
+
+// InterestRate returns the interest rate for the provided balance.
+func InterestRate(balance float64) float32 {
+	var rate float32 = 2.475
+	switch {
+	case balance < 0:
+		rate = 3.213
+	case balance >=0 && balance <1000:
+		rate = 0.5
+	case balance >= 1000 && balance < 5000:
+		rate = 1.621
+	}
+	return rate
+}
+
+// Interest calculates the interest for the provided balance.
+func Interest(balance float64) float64 {
+	return (float64(InterestRate(balance)) * balance) / 100
+}
+
+// AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
+func AnnualBalanceUpdate(balance float64) float64 {
+	return balance + Interest(balance)
+}
+
+// YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
+func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
+	years := 0
+	for {
+		if balance >= targetBalance {
+			break
+		}
+		balance = AnnualBalanceUpdate(balance)
+		years++
+	}
+	return years
+}
